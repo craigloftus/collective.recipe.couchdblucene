803669456fe4808798099920025f6c1b23701709
Michael Davis <m.r.davis@me.com>
2013-05-30 22:38:16 +0100

    Untar the file and put it in the lib folder

--- collective/recipe/couchdblucene/__init__.py
+++ collective/recipe/couchdblucene/__init__.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import zipfile
 
 class Recipe(object):
     """zc.buildout recipe"""
@@ -11,6 +12,14 @@ class Recipe(object):
 
     def install(self):
         """Installer"""
+        filename = 'couchdb-lucene-0.10.0-SNAPSHOT-dist.zip'
         os.chdir('parts/couchdb-lucene-download')
         subprocess.call('mvn')
+        os.chdir(os.path.join(os.getcwd(), os.path.pardir, os.path.pardir))
+        tar_file = zipfile.ZipFile(os.path.join(os.getcwd(), 'parts', 'couchdb-lucene-download', 'target', filename))
+        tar_file.extractall('lib')
+        os.chmod(os.path.join('lib', 'couchdb-lucene-0.10.0-SNAPSHOT', 'bin', 'run'), 0777)
+
+    def update(self):
+        pass
 
