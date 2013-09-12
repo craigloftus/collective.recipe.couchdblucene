389b3dca525d4dd8b357bfac5f32efc7a4de66dd
Craig Loftus <craigloftus@gmail.com>
2013-09-12 18:53:18 +0100

    Rewriting to do away with needing to know about version

--- collective/recipe/couchdblucene/__init__.py
+++ collective/recipe/couchdblucene/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import glob
 import os
+import shutil
 import subprocess
 import zipfile
 
@@ -8,18 +10,36 @@ class Recipe(object):
     """zc.buildout recipe"""
 
     def __init__(self, buildout, name, options):
-        pass
+        if not 'location' in options:
+            options['location'] = os.path.join(buildout['buildout']['directory'], 'lib', 'couchdb-lucene')
+
+        self.options = options
 
     def install(self):
         """Installer"""
-        filename = 'couchdb-lucene-0.10.0-SNAPSHOT-dist.zip'
-        os.chdir('parts/couchdb-lucene-download')
+        options = self.options
+        os.chdir(options['download'])
+        # Call Maven to build couchdb-lucene
         subprocess.call('mvn')
-        os.chdir(os.path.join(os.getcwd(), os.path.pardir, os.path.pardir))
-        tar_file = zipfile.ZipFile(os.path.join(os.getcwd(), 'parts', 'couchdb-lucene-download', 'target', filename))
-        tar_file.extractall('lib')
-        os.chmod(os.path.join('lib', 'couchdb-lucene-0.10.0-SNAPSHOT', 'bin', 'run'), 0777)
+        # If a path to the zip file is not provided, then look for it
+        if not 'zip' in options:
+            options['zip'] = glob.glob(os.path.join(options['download'], 'target', '*.zip'))[0]
+        # Create directory for extracted files
+        target_dir = options['location']
+        # Temporary directory
+        tmp_dir = '/tmp/'
+        # Unzip the produced archive
+        with zipfile.ZipFile(options['zip']) as zip_file:
+            # Extract
+            zip_file.extractall(tmp_dir)
+            # Find the run member
+            run_member = next(_ for _ in zip_file.namelist() if _.endswith('run'))
+            run_path = os.path.join(tmp_dir, run_member)
+            # Make the extracted run file executable
+            os.chmod(run_path, 0777)
+            # Move from the tmp directory to the target directory
+            shutil.move(os.path.join(tmp_dir, zip_file.namelist()[0][:-1]), target_dir)
+        return [target_dir]
 
     def update(self):
         pass
-
